c18bc2bdf45f3eb25c3232c1afb4c79a519e84c0
hyewon3938 <hyewon3938@naver.com>
2026-04-14 11:57:47 +0900

    chore(web): 불필요 파일 정리 + 리다이렉트 업데이트

--- web/src/app/backlog/page.tsx
+++ web/src/app/backlog/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function BacklogPage() {
-  redirect('/schedules');
+  redirect('/schedules/backlog');
 }
--- web/src/app/categories/page.tsx
+++ web/src/app/categories/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 
 export default function CategoriesPage() {
-  redirect('/schedules');
+  redirect('/schedules/categories');
 }
